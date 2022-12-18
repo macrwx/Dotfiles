3f90cb6bd1a17c4ffadd26bf4c3f0b450125b01e
maciek <eyzab@proton.me>
2022-12-18 16:59:14 +0100

    asdf

--- nvim/.config/nvim/lua/user/autocmd.lua
+++ nvim/.config/nvim/lua/user/autocmd.lua
@@ -1,7 +1,7 @@
 local group = vim.api.nvim_create_augroup("my_autocmds", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  group = group,
-  callback = function()
-      vim.lsp.buf.format()
-  end,
+    group = group,
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
